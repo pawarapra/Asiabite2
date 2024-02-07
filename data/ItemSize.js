44e5f72d7d970f6e27ba3a7a4ff9df141c0f2ebd
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2024-02-06 16:39:02 -0800

    data file

---
+++ data/ItemSize.js
@@ -0,0 +1,30 @@
+export default ItemSize = [
+    {
+        id:0,
+        name:'Regular Box (+10)',
+        sort:'+10',
+        description:'Perfect for solo snacking or a cozy couple treat.',
+        price: '$26.50',
+    },
+    {
+        id:1,
+        name:'Combo Box (+20)',
+        sort:'+20',
+        description:'Crafted for social moments and friendly gatherings.',
+        price: '$37.50',
+    },
+    {
+        id:2,
+        name:'Deluxe Box (+30)',
+        sort:'+30',
+        description:'Experience family-sized joy with our Deluxe Box.',
+        price: ' $54.50',
+    },
+    {
+        id:3,
+        name:'Office Mega Box (+150)',
+        sort:'+150',
+        description:'Tailored for the office pantry, keeping the team energized.',
+        price: '$175.50',
+    }
+];
